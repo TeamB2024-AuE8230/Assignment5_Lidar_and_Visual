ef2550de2f38872bd41477bab200665c7af9a776
Brandon Kelly <bkelly8@clemson.edu>
2024-03-20 23:17:13 -0400

    Added Blob tracking

--- assignment5a_LIDARwallfollowing/src/wall_follower_backup.py
+++ assignment5a_LIDARwallfollowing/src/wall_follower_backup.py
@@ -3,7 +3,7 @@
 import rospy
 import numpy as np
 from math import pi, radians
-
+LOOKAHEAD=330
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
@@ -20,7 +20,34 @@ class TurtleBot:
         print("Created Publisher")
         self.move_msg=Twist()
     def percieve(self, lidarData):
+        #self.left_wall = Wall(85,95,samples=5,data=lidarData)
+        self.right_wall = lidarData.ranges[LOOKAHEAD]#Wall(330,350,samples=5,data=lidarData)
+        print("Right Wall")
+        print(self.right_wall)
     def controller(self):
+        TARGET=.7
+        ## PID GAINS ##
+        P_GAIN = 5
+        I_GAIN = 2
+        D_GAIN = 2
+        K_ONE = P_GAIN + I_GAIN + D_GAIN#Gains for discrete PID
+        K_TWO = -P_GAIN + -2 * D_GAIN
+        K_THREE = D_GAIN
+        ## Error Signals##
+        self.errorSignal_2 = self.errorSignal_1#Errors for discrete PID
+        self.errorSignal_1 = self.errorSignal
+        self.errorSignal = TARGET - self.right_wall
+        ##Output Signal##
+        output=self.errorSignal * K_ONE + self.errorSignal_1 * K_TWO + self.errorSignal_2 * K_THREE#output signal for discrete PID
+        print("error:")
+        print(self.errorSignal)
+        # print("error1:")
+        # print(self.errorSignal_1)
+        # print("error2:")
+        # print(self.errorSignal_2)
+        print("output:")
+        print(output)
+        return output
     def forward(self):
         #Twist message for linear velocity components so turtlebot only drives forward
         self.move_msg.linear.x=0.15
@@ -43,14 +70,16 @@ class TurtleBot:
         self.percieve(lidarData)
         # print("Past Percieve")
         print("LIDAR Front:")
-        print(lidarData.ranges[0])
+        #print(lidarData.ranges[0])
+        #print(lidarData.ranges[LOOKAHEAD])
         if lidarData.ranges[0]<.15:
+            self.stop()
             self.backward()
-            pass
         else:
             self.forward()
+            self.steer()
         # print("Past Forward")
-        self.steer()
+        #self.steer()
         # print("Past steer")
         # print(self.move_msg)
         self.turtle_bot_move.publish(self.move_msg)
@@ -63,7 +92,7 @@ class TurtleBot:
         self.move_msg.linear.z=0.0
         self.turtle_bot_move.publish(self.move_msg)
 
-class Segment:#Class to seperate different segments of LIDAR data
+class Wall:
     def __init__(self,minAngle,maxAngle,samples,data):
         self.minAngle = minAngle#Defines the starting angle of a sweep
         self.maxAngle = maxAngle#Defines the end angle of a sweep for walls
